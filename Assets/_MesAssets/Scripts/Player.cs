ff56ae6e48814b779e714fb08337bed09e7213d9
Thomas DR <petittom222@gmail.com>
2023-02-17 14:46:59 -0500

    SauvegardePartie1

--- Assets/_MesAssets/Scripts/Player.cs
+++ Assets/_MesAssets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
 
     //Attrivuts 
-    [SerializeField] private float _vitesse = 15;
+    [SerializeField] private float _vitesse = 10;
 
     // Start is called before the first frame update
     private void Start()
