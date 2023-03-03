a57c27e6ead11f5e5ff7e641475f0480e6190c32
Thomas DR <petittom222@gmail.com>
2023-03-03 13:50:16 -0500

    scene1 finis

--- Assets/_MesAssets/Scripts/ZonePiege.cs
+++ Assets/_MesAssets/Scripts/ZonePiege.cs
@@ -8,14 +8,20 @@ public class ZonePiege : MonoBehaviour
     // Start is called before the first frame update
 
     private bool _estActive = false;
-    [SerializeField] private GameObject _piege = default;
-    private Rigidbody _rb;
+    //[SerializeField] private GameObject _piege = default;
+    [SerializeField] private List<GameObject> _listePieges = new List<GameObject>();
+    //private Rigidbody _rb;
+    private List<Rigidbody> _listeRb = new List<Rigidbody>(); 
     [SerializeField] private float _intensiteForce = 500;
 
 
     private void Start()
     {
-        _rb = _piege.GetComponent<Rigidbody>();
+        foreach(var piege in _listePieges)
+        {
+            _listeRb.Add(piege.GetComponent<Rigidbody>());
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,8 +30,11 @@ public class ZonePiege : MonoBehaviour
         {
             _estActive = true;
             Debug.Log("Piege Activer, gotem");
-            _rb.useGravity= true;
-            _rb.AddForce(Vector3.down * _intensiteForce);
+            foreach(var rb in _listeRb)
+            {
+                rb.useGravity= true;
+                rb.AddForce(Vector3.down * _intensiteForce);
+            }
 
         }
         
